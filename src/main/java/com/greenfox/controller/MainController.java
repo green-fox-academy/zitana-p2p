fb6e26adb3883a708c084d3742fe757d76f72cb9
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-05-17 13:42:31 +0200

    initialise Log class

--- src/main/java/com/greenfox/controller/MainController.java
+++ src/main/java/com/greenfox/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.greenfox.controller;
 
+import com.greenfox.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +9,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+  @Autowired
+  MessageRepository messageRepository;
+
+  @RequestMapping("/")
+  public String main() {
+    return "main";
+  }
+
   @RequestMapping(value = "/")
   public String log(Model model){
     System.out.println(System.getenv("CHAT_APP_LOGLEVEL"));
-    return "index";
+    return "main";
   }
 }
