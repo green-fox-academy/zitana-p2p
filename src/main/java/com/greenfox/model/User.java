522eafdd246ea6c1d8db75f22a89ac4426412e25
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-05-17 17:42:45 +0200

    name table

--- src/main/java/com/greenfox/model/User.java
+++ src/main/java/com/greenfox/model/User.java
@@ -5,10 +5,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @Setter
 @Getter
 public class User {
