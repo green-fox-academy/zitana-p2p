a009566e5e1420545be604b123a50d54e77eb659
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-05-23 18:23:17 +0200

    add resttemplate to restcontroller

--- src/main/java/com/greenfox/controller/MainRestController.java
+++ src/main/java/com/greenfox/controller/MainRestController.java
@@ -1,5 +1,6 @@
 package com.greenfox.controller;
 
+import com.greenfox.model.Client;
 import com.greenfox.model.Receive;
 import com.greenfox.model.Response;
 import com.greenfox.repository.MessageRepository;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MainRestController {
@@ -23,6 +25,8 @@ public class MainRestController {
   @CrossOrigin("*")
   public Response receive(@RequestBody Receive receive) {
     messageRepository.save(receive.getMessage());
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.postForObject(System.getenv("CHAT_APP_PEER_ADDRESS") + "/api/message/receive", receive, Response.class);
     return messageValidator.validate(receive);
   }
 }
