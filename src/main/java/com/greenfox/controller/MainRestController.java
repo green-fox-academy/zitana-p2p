4aab71832871a0c4bcb6f5d13f4924e5f87cb7ec
Zita Nagyaghy <nagyaghy.zita@gmail.com>
2017-05-24 10:37:36 +0200

    add Log to rest controller

--- src/main/java/com/greenfox/controller/MainRestController.java
+++ src/main/java/com/greenfox/controller/MainRestController.java
@@ -1,10 +1,11 @@
 package com.greenfox.controller;
 
-import com.greenfox.model.Client;
+import com.greenfox.model.Log;
 import com.greenfox.model.Receive;
 import com.greenfox.model.Response;
 import com.greenfox.repository.MessageRepository;
 import com.greenfox.service.MessageValidator;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +24,8 @@ public class MainRestController {
 
   @RequestMapping("/api/message/receive")
   @CrossOrigin("*")
-  public Response receive(@RequestBody Receive receive) {
+  public Response receive(HttpServletRequest request, @RequestBody Receive receive) {
+    System.out.println(new Log(request.getMethod(), request.getRequestURI(), "received message=" + receive));
     messageRepository.save(receive.getMessage());
     RestTemplate restTemplate = new RestTemplate();
     restTemplate.postForObject(System.getenv("CHAT_APP_PEER_ADDRESS") + "/api/message/receive", receive, Response.class);
